741ba39027749a125247f3c576bf4fe3c181f21b
HernandezZuly <zulballes023@gmail.com>
2024-05-21 14:11:00 -0500

    cambios

--- front/pages/teamScrum/create.js
+++ front/pages/teamScrum/create.js
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { createTeamScrum } from "../../src/app/services/teamScrumService";
-import styles from "../teamScrum.css";
+import styles from "../../styles/teamScrum.css"
 
 const CreateTeamScrum = () => {
     const { register, handleSubmit, reset } = useForm();
@@ -14,28 +14,28 @@ const CreateTeamScrum = () => {
     };
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Nuevo Proyecto</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className={styles.inputWrapper}>
-                    <label htmlFor="nameProject" className={styles.label}>Nombre del proyecto</label>
+        <div className="container">
+            <h1 className="title">Nuevo Proyecto</h1>
+            <form className="formC" onSubmit={handleSubmit(onSubmit)}>
+                <div className="inputWrapper">
+                    <label htmlFor="nameProject" className="label">Nombre del proyecto</label>
                     <input 
                         {...register('nameProject', { required: true })}
                         id="nameProject"
-                        className={styles.input}
+                        className="input"
                     />
                 </div>
-                <div className="flex justify-between">
+                <div className="contButton">
                     <button 
                         type="button"
-                        className={`${styles.button} ${styles.cancelButton}`}
+                        className="button cancelButton"
                         onClick={() => router.push('/teams-scrum')}
                     >
                         Cancelar
                     </button>
                     <button 
                         type="submit"
-                        className={`${styles.button} ${styles.registerButton}`}
+                        className="button registerButton"
                     >
                         Registrar
                     </button>
