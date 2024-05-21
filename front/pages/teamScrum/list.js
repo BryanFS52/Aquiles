0db56bf953e4b5bda99e4a026dbd76c792b27688
keishla <keishlanayedcamargorojas@gmail.com>
2024-05-21 17:54:27 -0500

    Cambios diseño

--- front/pages/teamScrum/list.js
+++ front/pages/teamScrum/list.js
@@ -1,10 +1,41 @@
-const ListTeamScrum = () => {
+import React, { useState } from 'react';
+import styles from "../../styles/teamScrum.css"
 
-    return (
-        <div>
-            <h1>Aqui se va listar</h1>
+const ListaProject = () => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleClick = (item) => {
+    setActiveItem(item);
+  };
+
+  return (
+
+    <div>
+        <h1 className="title">Proyectos</h1>
+        
+        <br></br><br></br>
+
+    <nav className="menu">
+    <div className="inputWrapper">
+                    <label htmlFor="nameProject" className="label">Proyectos Formativos (C.S.F)</label>
         </div>
-    );
-}
+      <ul>
+        <ul>
+          <a href="/asistencia" onClick={() => handleClick('asistencia')}>Asistencia</a>
+        </ul>
+        <ul>
+          <a href="/teams" onClick={() => handleClick('teams')}>Teams</a>
+        </ul>
+        <ul>
+          <a href="/aprendices" onClick={() => handleClick('aprendices')}>Aprendices</a>
+        </ul>
+        <ul>
+          <a href="/proyectos" onClick={() => handleClick('proyectos')}>Proyectos</a>
+        </ul> 
+      </ul>
+    </nav>
+    </div>
+  );
+};
 
-export default ListTeamScrum;
+export default ListaProject;
