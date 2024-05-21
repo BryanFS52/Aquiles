fbfefda03908724d67025fa1c87bb5df22a1ac00
HernandezZuly <zulballes023@gmail.com>
2024-05-21 17:21:08 -0500

    cambios en el estilo del formulario de crear team

--- front/pages/teamScrum/create.js
+++ front/pages/teamScrum/create.js
@@ -14,29 +14,18 @@ const CreateTeamScrum = () => {
     };
 
     return (
-        <div className="container">
-            <h1 className="title">Nuevo Proyecto</h1>
-            <form className="formC" onSubmit={handleSubmit(onSubmit)}>
-                <div className="inputWrapper">
-                    <label htmlFor="nameProject" className="label">Nombre del proyecto</label>
-                    <input 
-                        {...register('nameProject', { required: true })}
-                        id="nameProject"
-                        className="input"
-                    />
+        <div className="containerCreate">
+            <h1 className="titleCreate">Nuevo Proyecto</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="cajaInput">
+                    <label htmlFor="nameProject" className="label1">Nombre del proyecto</label>
+                    <input {...register('nameProject', { required: true })} id="nameProject" className="input" />
                 </div>
-                <div className="contButton">
-                    <button 
-                        type="button"
-                        className="button cancelButton"
-                        onClick={() => router.push('/teams-scrum')}
-                    >
+                <div className="contButtonCreate">
+                    <button type="button" className="buttonCreate cancelButton" onClick={() => router.push('/list')} >
                         Cancelar
                     </button>
-                    <button 
-                        type="submit"
-                        className="button registerButton"
-                    >
+                    <button type="submit" className="buttonCreate registerButton">
                         Registrar
                     </button>
                 </div>
