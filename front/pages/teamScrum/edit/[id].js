c4538e62e29d3bdf8bcb804a4c055d8b1ef48bc5
HernandezZuly <zulballes023@gmail.com>
2024-05-22 17:00:05 -0500

    parte front editar team terminada

--- front/pages/teamScrum/edit/[id].js
+++ front/pages/teamScrum/edit/[id].js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { updateTeamScrum, getTeamScrumById } from "../../../src/app/services/teamScrumService";
 import styles from "../../../resources/styles/teamScrum.css"
+import candado from "../../../resources/img/iconoCandado.png"
 
 const EditTeamScrum = ({id}) => {
     const { register, handleSubmit, reset, setValue } = useForm();
@@ -14,20 +15,22 @@ const EditTeamScrum = ({id}) => {
             getTeamScrumById(id)
             .then(data => {
                 setTeamData(data);
+                setValue('nameProject', data.nameProject); // Inicializar el valor del formulario
+                setValue('team_scrum_id', data.team_scrum_id); // Inicializar el valor del formulario
             })
             .catch(error => {
                 console.error('Error al obtener datos:', error);
             });
         };
-        
+
         fetchData();
-    }, [id]);
+    }, [id, setValue]);
 
     const onSubmit = async (data) => {
         data.team_scrum_id = id; // Asegura que el ID esta incluido en los datos
         await updateTeamScrum(data);
         reset();
-        router.push('/list');
+        router.push('../list');
     };
 
     return (
@@ -36,18 +39,21 @@ const EditTeamScrum = ({id}) => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="cajaInput">
                     <label htmlFor="nameProject" className="label1">Nombre del proyecto</label>
-                    <input {...register('nameProject', { required: true })} id="nameProject" className="input" value={teamData.nameProject || 'No se muestra el nombre'} />
+                    <input {...register('nameProject', { required: true })} id="nameProject" className="input" />
                 </div>
                 <div className="cajaInput">
-                    <label htmlFor="team_scrum_id" className="label2">Asignar numero de proyecto proyecto</label>
-                    <input {...register('team_scrum_id')} id="team_scrum_id" className="input" readOnly value={teamData.team_scrum_id || 'No se muestra el id'} />
+                    <label htmlFor="team_scrum_id" className="label2">Número de proyecto</label>
+                    <div className="inputIcon">
+                        <input {...register('team_scrum_id')} id="team_scrum_id" className="input2" readOnly />
+                        <img src={candado.src} alt="candado" className="icon" />
+                    </div>
                 </div>
                 <div className="contButtonCreate">
-                    <button type="button" className="buttonCreate cancelButton" onClick={() => router.push('/list')} >
+                    <button type="button" className="buttonCreate cancelButton" onClick={() => router.push('../list')} >
                         Cancelar
                     </button>
                     <button type="submit" className="buttonCreate registerButton">
-                        Guardar Cambios
+                        Actualizar
                     </button>
                 </div>
             </form>
