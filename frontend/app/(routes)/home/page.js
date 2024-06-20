222a8c1dbdab3b3562df08f0c3f8994e8f9ed604
jorgeDev <jhorspineda30@gmail.com>
2024-06-20 17:56:20 -0500

    Actualización de icono de basura, aumento de tamaño en los iconos y un borrador para hacer el triangulo

--- frontend/app/(routes)/home/page.js
+++ frontend/app/(routes)/home/page.js
@@ -1,43 +1,56 @@
 import Link from "next/link";
 import { Header } from "../../components/header";
 import { Sidebar } from "../../components/sidebar";
+import { MdAddCircle } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
 import { MdAdd } from "react-icons/md";
-import { GoChecklist } from "react-icons/go";
 
-export default function Home () {
-    return(
-    <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
-      <Sidebar/>
-    <div className="xl:col-span-5 ">
-      <Header/>
+export default function Home() {
+	return (
+		<div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
+			<Sidebar />
+			<div className="xl:col-span-5">
+				<Header />
 
-    <div className="h-[90vh] overflow-y-scroll p-12 inline-block w-full">
-        <h1 className="font-serif text-4xl pb-3 border-b-2 border-gray-400 w-1/2">Teams Scrums </h1>
-        <br/>
-        
-        <li className="h-9 w-14 flex items-center justify-center border-2 rounded-lg bg-cyan-900 hover:bg-green-600  ml-auto">
-                    <a href="#">
-                        {""}
-                        <MdAdd className="w-8 h-8"/>
-                    </a>
-        </li>
+				<div className="h-[90vh] overflow-y-scroll p-12 inline-block w-full relative">
+					<h1 className="font-serif text-4xl pb-3 border-b-2 border-gray-400 w-1/2">
+						Teams Scrums{" "}
+					</h1>
+					<br />
 
-        <div class="w-1/3 h-56 rounded-lg overflow-hidden shadow-lg bg-zinc-200 ">
-        <div class="px-6 py-4">
-          <div class="font-serif text-xl mb-2">Nombre del Proyecto</div>
-              <p class="text-black-700 text-base text-xs">Desarrollo de la Aplicación Móvil</p><br/>
-              <div class="font-serif text-xl mb-2">Team Número</div>
-              <p class="text-black-700 text-base text-xs">Equipo 5</p><br/>
-              <div class="font-serif text-xl mb-2 flexbox">Agregar Información
-              <Link href="/home" className="ml-2">
-              <GoChecklist className="inline-block text-xl"/>
-              </Link>
-              </div>
-          </div>  
-          </div>
-      </div>
-    </div>
-  </div>
+					<li className="h-9 w-14 flex items-center justify-center border-2 rounded-lg bg-cyan-900 hover:bg-green-600 ml-auto">
+						<a href="#">
+							{""}
+							<MdAdd className="w-8 h-8" />
+						</a>
+					</li>
 
-    );
+					<div className="w-1/3 h-56 rounded-lg overflow-hidden shadow-lg bg-zinc-200 relative">
+          <div className="triangle absolute top-0 right-0 transform rotate-45 h-8 w-8 bg-cyan-900"></div>
+						<div className="px-6 py-4">
+							<div className="font-serif text-xl mb-2">Nombre del Proyecto</div>
+							<p className="text-black-700 text-base text-xs">
+								Desarrollo de la Aplicación Móvil
+							</p>
+							<br />
+							<div className="font-serif text-xl mb-2">Team Número</div>
+							<p className="text-black-700 text-base text-xs">Equipo 5</p>
+							<br />
+							<div className="font-serif text-xl mb-2 flex">
+								Agregar Información
+
+                
+								<Link href="/home" className="ml-2">
+									<MdAddCircle className="inline-block text-2xl text-cyan-900" />
+								</Link>
+								<Link href="/home" className="ml-2 ml-auto">
+									<FaTrashAlt className="inline-block text-2xl text-cyan-900" />
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
