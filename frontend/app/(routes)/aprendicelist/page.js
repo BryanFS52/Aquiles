ff8dc1112812ddb32c33f491ef047809c57568dc
Keishla12 <keishlanayedcamargorojas@gmail.com>
2024-08-06 12:32:51 -0500

    Implementacion de alertas en correo electronico

--- frontend/app/(routes)/aprendicelist/page.js
+++ frontend/app/(routes)/aprendicelist/page.js
@@ -1,12 +1,14 @@
 "use client"
 
 import React, { useState } from "react";
-import { Header } from "../../components/header"; //importaciones del header y del sidebar para hacer el llamado
+import { Header } from "../../components/header";
 import { Sidebar } from "../../components/sidebar";
 import { PiStudentFill } from "react-icons/pi";
 import { ImMail4 } from "react-icons/im";
 import ModalCorreo from "../../components/Modals/modalCorreo";
-import axios from 'axios';
+import { sendEmailAbsence } from "../../services/emailService";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function AprendicesList() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,122 +20,116 @@ export default function AprendicesList() {
     };
 
     const handleSendEmail = async () => {
+        if (!selectedStudent) return;
+
         try {
-            const response = await axios.post('/api/send-email', {
-                studentName: selectedStudent.name,
-                date: selectedStudent.date
-            });
-            if (response.status === 200) {
-                alert("Correo enviado con éxito");
-            } else {
-                alert("Hubo un error al enviar el correo");
-            }
+            await sendEmailAbsence(selectedStudent.email);
+            toast.success("Correo enviado con éxito");
         } catch (error) {
             console.error("Error al enviar el correo:", error);
-            alert("Hubo un error al enviar el correo");
+            toast.error("Hubo un error al enviar el correo");
+        } finally {
+            setIsModalOpen(false);
         }
-        setIsModalOpen(false);
     };
 
-
-    return(
-
+    return (
         <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
-        <Sidebar />
-        <div className="xl:col-span-5">
-          <Header />
-        <div className="h-[90vh] overflow-y-scroll p-12 inline-block w-full relative bg-neutral-100 space-y-5 ">
-          <h1 className="font-serif text-4xl pb-3 border-b-2 border-black w-80">Lista de Asistencia</h1>
-            <div className="flex px-9 space-x-24">
+            <Sidebar />
+            <div className="xl:col-span-5">
+                <Header />
 
-            <div className=" flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
-                  <div className="z-50 justify-end space-y-3">
-                  <PiStudentFill className="w-9 h-9 text-stone-600 ml-6"/><br/>
-
-                  <div>
-                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">25</span><br/><br/>
-                    <span className="font-serif text-lg font-normal ">Aprendices de la ficha</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className=" flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
-                  <div className="z-50 justify-end space-y-3">
-                  <PiStudentFill className="w-9 h-9 text-stone-600 ml-6"/><br/>
-
-                  <div>
-                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">20</span><br/><br/>
-                    <span className="font-serif text-lg font-normal ">Aprendices en clase</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className=" flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
-                  <div className="z-50 justify-end space-y-3">
-                  <PiStudentFill className="w-9 h-9 text-stone-600 ml-6"/><br/>
-
-                  <div>
-                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">5</span><br/><br/>
-                    <span className="font-serif text-lg font-normal ">Aprendices que fallaron</span>
-                  </div>
-                </div>
-              </div>
-              <ModalCorreo isOpen={isModalOpen} onClose={toggleModal} />
-                </div>
-                <div className="flex w-1/2 h-auto rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative ml-80 p-4 ">
-                    <div className="z-50 justify-end space-y-3">
-                    </div>
-
-                    {/* Tabla con los datos quemados */}
-                    <div className="overflow-x-auto w-full">
-                        <table className="ml-16 bg-white w-96 md:w-5/6">
-                                <thead>
-                                <tr>
-                                    <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">Número de Documento</th>
-                                    <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">Nombres y Apellidos</th>
-                                    <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">L</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
-                                </tr>
-
-                                <tr>
-                                <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700"> 10078459687 </td>
-                                <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700"> Michael Felipe Laiton Chaparro </td>
-                                <td className="px-4 py-2 border-2 border-gray-200 text-sm text-red-600 font-semibold"> X </td>
-                                <td className="cursor-pointer" onClick={toggleModal}>
-                                  <ImMail4 className="w-6 h-6 ml-2" />
-                                </td>
-                              </tr>
-
-                                <tr>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
-                                    <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
-                                </tr>
-                                </tbody>
-                        </table>
-                        <div className="flex justify-end mr-12">
-                        <button type="button" className="text-white font-serif h-11 w-44 rounded-lg text-sm px-5 my-6 ml-80 bg-custom-blue dark:hover:bg-custom-blue dark:focus:ring-custom-blue flex items-center"
-                            >
-                            Guardar Asistencia
-                        </button>
+                <div className="h-[90vh] overflow-y-scroll p-12 inline-block w-full relative bg-neutral-100 space-y-5 ">
+                    <h1 className="font-serif text-4xl pb-3 border-b-2 border-black w-80">Lista de Asistencia</h1>
+                    <div className="flex px-9 space-x-24">
+                        <div className="flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
+                            <div className="z-50 justify-end space-y-3">
+                                <PiStudentFill className="w-9 h-9 text-stone-600 ml-6" /><br />
+                                <div>
+                                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">25</span><br /><br />
+                                    <span className="font-serif text-lg font-normal ">Aprendices de la ficha</span>
+                                </div>
+                            </div>
                         </div>
+
+                        <div className="flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
+                            <div className="z-50 justify-end space-y-3">
+                                <PiStudentFill className="w-9 h-9 text-stone-600 ml-6" /><br />
+                                <div>
+                                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">20</span><br /><br />
+                                    <span className="font-serif text-lg font-normal ">Aprendices en clase</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex w-96 h-48 rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative mb-4 p-4 ">
+                            <div className="z-50 justify-end space-y-3">
+                                <PiStudentFill className="w-9 h-9 text-stone-600 ml-6" /><br />
+                                <div>
+                                    <span className="text-5xl font-semibold font-kiwi-marumaru ml-6">5</span><br /><br />
+                                    <span className="font-serif text-lg font-normal ">Aprendices que fallaron</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <ModalCorreo isOpen={isModalOpen} onClose={() => toggleModal(null)} onSendEmail={handleSendEmail} />
                     </div>
-                </div>
-        </div>
-        </div>
-        </div>
-    
-    );
+
+                    <div className="flex w-1/2 h-auto rounded-lg overflow-hidden shadow-lg bg-white border-2 border-gray-300 relative ml-80 p-4 ">
+                       <div className="z-50 justify-end space-y-3">
+                       </div>
+
+                       <div className="overflow-x-auto w-full">
+                           <table className="ml-16 bg-white w-96 md:w-5/6">
+                               <thead>
+                                   <tr>
+                                       <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">Número de Documento</th>
+                                       <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">Nombres y Apellidos</th>
+                                       <th className="px-4 py-2 border-2 border-gray-200 bg-gray-100 text-sm font-semibold text-gray-700">L</th>
+                                   </tr>
+                               </thead>
+                               <tbody>
+                                   <tr>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
+                                       <td className="cursor-pointer" onClick={() => toggleModal({ email: 'jeissonvargas74@gmail.com' })}>
+                                          
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
+                                       <td className="cursor-pointer" onClick={() => toggleModal({ email: 'jeissonvargas74@gmail.com' })}>
+                                       
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-red-600 font-semibold">X</td>
+                                       <td className="cursor-pointer" onClick={() => toggleModal({ email: 'jeissonvargas74@gmail.com' })}>
+                                           <ImMail4 className="w-6 h-6 ml-2" />
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">10078459687</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-gray-700">Michael Felipe Laiton Chaparro</td>
+                                       <td className="px-4 py-2 border-2 border-gray-200 text-sm text-green-600 font-semibold">✓</td>
+                                   </tr>
+                               </tbody>
+                           </table>
+                           <div className="flex justify-end mr-12">
+                               <button type="button" className="text-white text-center font-serif h-11 w-40 mr-6 rounded-lg text-sm px-5 my-6 ml-80 bg-custom-blue dark:hover:bg-custom-blue dark:focus:ring-custom-blue flex items-center">
+                                   Guardar Asistencia
+                               </button>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <ToastContainer />
+       </div>
+   );
 }
--- frontend/app/components/Modals/modalCorreo.js
+++ frontend/app/components/Modals/modalCorreo.js
@@ -1,31 +1,32 @@
-import React from 'react';
+"use client"
+
+import React from "react";
 
 const ModalCorreo = ({ isOpen, onClose, onSendEmail }) => {
-  if (!isOpen) return null;
+    if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-      {/* Fondo opaco gris */}
-      <div className="fixed inset-0 bg-cyan-900 opacity-35"></div>
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="fixed inset-0 flex items-center justify-center z-50 bg-custom-blue bg-opacity-50">
+                <div className="bg-white w-1/4 h-72 border-2 border-gray-400 rounded-lg">
+                <h2 className="text-2xl mb-4 text-center pt-9 font-medium">¿Está Seguro de enviar el Correo Electronico?</h2>
 
-      <div className="relative w-full max-w-md mx-auto my-12 bg-white rounded-lg shadow-lg">
-        <div className="p-5">
-          <div className='flex justify-center items-center'>
-            <h1 className="text-2xl font-serif font-medium text-center">¿Está seguro de enviar el correo electrónico?</h1>
-          </div>
-          <div className='flex mt-10 mr-2 space-x-16 justify-center py-7'>
-            <button className='bg-red-600 hover:bg-red-600 border-2 border-red-800 text-white text-lg font-medium py-4 px-14 rounded' onClick={onClose}>
-              NO
-            </button>
+                <button
+                    onClick={onClose}
+                    className="ml-14 text-white font-semibold text-xl font-serif h-14 w-36 rounded-lg px-5 my-6 bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-600 border-2 border-red-700">
+                    NO
+                </button>
 
-            <button className='bg-green-500 hover:bg-green-500 border-2 border-green-700 text-white text-lg font-medium py-4 px-14 rounded' onClick={onSendEmail}>
-              SÍ
-            </button>
-          </div>
+
+                <button
+                    onClick={onSendEmail}
+                    className="ml-14 text-white font-semibold font-serif h-14 w-36 rounded-lg text-xl px-5 my-6 bg-green-600 dark:hover:bg-green-600 dark:focus:ring-green-400 border-2 border-green-700">
+                    SI
+                </button>
+            </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default ModalCorreo;
