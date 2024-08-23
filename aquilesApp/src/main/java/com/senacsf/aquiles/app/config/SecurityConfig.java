8d92ef510f7385de74eb4766548c3af29edab9db
Keishla12 <keishlanayedcamargorojas@gmail.com>
2024-08-23 08:56:53 -0500

    cambios en los archivos security config y EmailController

--- aquilesApp/src/main/java/com/senacsf/aquiles/app/config/SecurityConfig.java
+++ aquilesApp/src/main/java/com/senacsf/aquiles/app/config/SecurityConfig.java
@@ -18,13 +18,22 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para desarrollo
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
+<<<<<<< HEAD
                                 .anyRequest().permitAll() // Permitir todas las solicitudes sin autenticación
+=======
+                                .requestMatchers("/api/send-notification").permitAll() // Permitir acceso sin autenticación a este endpoint
+                                .requestMatchers("/api/teams-scrum/all").permitAll()
+                                .requestMatchers("/api/teams-scrum/create").permitAll()
+                                .requestMatchers("/api/teams-scrum/update").permitAll()
+                                .requestMatchers("/api/teams-scrum/delete/{id}").permitAll()
+                                .anyRequest().authenticated() // Requerir autenticación para cualquier otra solicitud
+>>>>>>> 8735888 (cambios en los archivos security config y EmailController)
                 );
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Proporciona una implementación de codificación de contraseñas basada en BCrypt
+        return new BCryptPasswordEncoder(); // Codificación de contraseñas con BCrypt
     }
 }
--- aquilesApp/src/main/java/com/senacsf/aquiles/app/request/EmailRequest.java
+++ aquilesApp/src/main/java/com/senacsf/aquiles/app/request/EmailRequest.java
@@ -10,4 +10,8 @@ public class EmailRequest {
     private String subject;
     private String htmlContent;
 
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 8735888 (cambios en los archivos security config y EmailController)
