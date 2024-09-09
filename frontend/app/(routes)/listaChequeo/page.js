595a93fbdbf7cd6263fcd660c1d4dce8fde53d80
JhorsPineda <jhorspineda30@gmail.com>
2024-09-09 17:34:20 -0500

    actualización de lista de chequeo

--- frontend/app/(routes)/listaChequeo/page.js
+++ frontend/app/(routes)/listaChequeo/page.js
@@ -4,11 +4,7 @@ import React, { useState, useRef } from "react";
 import { Header } from "../../components/header"; //importaciones del header y del sidebar para hacer el llamado
 import { downloadReportPDF } from "../../services/PDFService";
 import { Sidebar } from "../../components/sidebar";
-import { BiX } from "react-icons/bi";
-import { BiCheck } from "react-icons/bi";
-import CheckListTable from "../../components/CheckListTable";
-import { ToastContainer, toast } from "react-toastify"; //importacion de la libreria ToastContainer para las alertas con la animacion 
-import 'react-toastify/dist/ReactToastify.css';
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function ListaChequeo() {
     // Opciones para los select 
@@ -18,6 +14,7 @@ export default function ListaChequeo() {
     const [team, setTeam] = useState (["Team 1", "Team 2", "Team 3"]);
     const [pilar, setPilar] = useState (["Componente Técnico", "Componente Humanístico", "Componente Comunicación", "Componente Emprendimiento", "Componente Inglés"]);
 
+     // Refs para inputs de archivos
   const fileInputRefPrev = useRef(null);
   const fileInputRefNew = useRef(null);
 
@@ -42,6 +39,7 @@ export default function ListaChequeo() {
         setAttendees(updatedAttendees);
     };
 
+        // Funciones para abrir el explorador de archivos
     const handleUploadPrev = () => {
         fileInputRefPrev.current.click();
     };
@@ -49,6 +47,8 @@ export default function ListaChequeo() {
         fileInputRefNew.current.click();
     };
 
+
+    
     return (
         <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
             <Sidebar />
@@ -64,7 +64,7 @@ export default function ListaChequeo() {
                                 <div>
                                     <span className="text-custom-blue text-base font-semibold font-inter">Ficha:</span>
                                     <div className="relative inline-block ml-8">
-                                        <input type="text" className="border-2 border-gray-300 rounded-lg ml-4 w-52"/>
+                                        <input type="text" className="border-2 border-gray-300 rounded-lg ml-5 w-52"/>
                                     </div>
                                 </div>
 
@@ -72,7 +72,6 @@ export default function ListaChequeo() {
                                     <span className="text-custom-blue text-base font-semibold font-inter">Sede:</span>
                                     <div className="relative inline-block ml-8">
                                         <select className="border-2 border-gray-300 rounded-lg ml-5 w-52">
-                                        <option value=""> </option> 
                                             {sede.map((sede, index) => (
                                                 <option key={index} value={sede}>{sede}</option>
                                             ))}
@@ -83,8 +82,7 @@ export default function ListaChequeo() {
                                 <div>
                                     <span className="text-custom-blue text-base font-semibold font-inter">Jornada:</span>
                                     <div className="relative inline-block ml-8">
-                                        <select className="border-2 border-gray-300 rounded-lg mr- w-52">
-                                        <option value=""> </option> 
+                                        <select className="border-2 border-gray-300 rounded-lg w-52">
                                             {jornada.map((jornada, index) => (
                                                 <option key={index} value={jornada}>{jornada}</option>
                                             ))}
@@ -99,7 +97,6 @@ export default function ListaChequeo() {
                                         <span className="text-custom-blue text-base font-semibold font-inter">Trimestre:</span>
                                         <div className="relative inline-block ml-3">
                                             <select className="border-2 border-gray-300 rounded-lg ml-5 w-52">
-                                            <option value=""> </option> 
                                                 {trimestre.map((trimestre, index) => (
                                                     <option key={index} value={trimestre}>{trimestre}</option>
                                                 ))}
@@ -110,8 +107,7 @@ export default function ListaChequeo() {
                                 <div>
                                     <span className="text-custom-blue text-base font-semibold font-inter">Team:</span>
                                     <div className="relative inline-block ml-10">
-                                        <select className="border-2 border-gray-300 rounded-lg ml-6 w-52">
-                                        <option value=""> </option> 
+                                        <select className="border-2 border-gray-300 rounded-lg ml-5 w-52">
                                             {team.map((team, index) => (
                                                 <option key={index} value={team}>{team}</option>
                                             ))}
@@ -121,8 +117,7 @@ export default function ListaChequeo() {
                                 <div>
                                     <span className="text-custom-blue text-base font-semibold font-inter">Pilar:</span>
                                     <div className="relative inline-block ml-16">
-                                        <select className="border-2 border-gray-300 rounded-lg ml-1 w-52">
-                                        <option value=""> </option> 
+                                        <select className="border-2 border-gray-300 rounded-lg w-52">
                                             {pilar.map((pilar, index) => (
                                                 <option key={index} value={pilar}>{pilar}</option>
                                             ))}
@@ -130,7 +125,9 @@ export default function ListaChequeo() {
                                     </div>
                                 </div>
                                 <div className="pt-1">
-                                    <button className="bg-custom-blue text-white text-center font-bold py-2 px-4 rounded-lg mt-4 block ml-3 w-28 h-10">Buscar</button>
+                                    <button className="bg-custom-blue text-white text-center font-bold py-2 px-4 rounded-lg mt-4 block ml-3 w-28 h-10">
+                                        Buscar
+                                    </button>
                                 </div>
                             </div>
                             
@@ -138,21 +135,19 @@ export default function ListaChequeo() {
                                 <div className="w-52 h-28 bg-white border border-gray-200 p-4 rounded-lg shadow-md">
                                     <h2 className="text-gray-700 text-sm font-bold mb-2 text-center">Exportar lista de chequeo como:</h2>
                                     <div className="flex space-x-2 mb-4">
-                                        <button type="button" onClick={handleDownloadPDF} className="w-full bg-custom-blue font-bold py-2 px-4 rounded text-white focus:outline-none focus:shadow-outline">PDF</button>
-                                        <button className="w-full bg-custom-blue font-bold py-2 px-4 rounded text-white focus:outline-none focus:shadow-outline">Excel</button>
+                                        <button type="button" onClick={handleDownloadPDF} className="w-full bg-custom-blue font-bold py-2 px-4 rounded text-white focus:outline-none focus:shadow-outline">
+                                            PDF
+                                        </button>
+                                        <button className="w-full bg-custom-blue font-bold py-2 px-4 rounded text-white focus:outline-none focus:shadow-outline">
+                                            Excel
+                                        </button>
                                     </div>
                                     <div className="w-44 h-20 bg-white border border-gray-200 p-4 rounded-lg shadow-md mr-10">
                                         <h2 className="text-gray-700 text-sm font-bold mb-2">Indicaciones de uso:</h2>
-                                        <div className="flex space-x-7 items-center">
-                                        <div className="flex items-center space-x-1">
-                                            <span className="ml-2">Si</span>
-                                            <BiCheck className="text-green-500 text-3xl" />
+                                        <div className="flex space-x-7">
+                                            <span className="ml-3">Si ✅</span>
+                                            <span className="ml-3">No ❌</span>
                                         </div>
-                                        <div className="flex items-center space-x-1">
-                                            <span className="ml-2">No</span>
-                                            <BiX className="text-red-600 text-3xl" />
-                                        </div>
-                                    </div>
                                     </div>  
                                 </div>
                             </div>
@@ -160,24 +155,123 @@ export default function ListaChequeo() {
                     </div>
                 </div>
 
-            <CheckListTable />
+                <div className="flex bg-white w-full h-14">
+            </div>
+
+            <div className="container w-2/3 ml-52">
+                <table className="min-w-full divide-y divide-gray-200 border border-gray-200 table-auto">
+                <thead className="bg-sky-950">
+                  
+                    <tr>
+                        
+                        <th className="px-4 py-3 w-20 border-2 border-gray-300 bg-sky-950 text-sm font-semibold text-white">Item</th>
+                        <th className="px-4 w-96 border-2 border-gray-300 bg-sky-950 text-sm font-semibold text-white text-center">Indicadores y/o Variables</th>
+                        <th className="px-4 w-20 border-2 border-gray-300 bg-sky-950 text-sm font-semibold text-white">Si</th>
+                        <th className="px-4 w-20 border-2 border-gray-300 bg-sky-950 text-sm font-semibold text-white">No</th>
+                        <th className="px-4 border-2 border-gray-300 bg-sky-950 text-sm font-semibold text-white">Observaciones</th>
+
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-300">
+                        <tr>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm text-center">1</td>
+                        <td className="px-2 border-2 border-gray-300 text-sm text-center">El software evidencia autenticación y manejo dinámico de roles.</td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item1" className="mr-2 ml-4 transform scale-150 custom-checkbox" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item2" className="mr-2 ml-4 transform scale-150 custom-checkboxred" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm"><span className="text-red-500 font-bold"></span></td>
+                    </tr>
+
+                    <tr>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm text-center">1</td>
+                        <td className="px-2 border-2 border-gray-300 text-sm text-center">El software evidencia autenticación y manejo dinámico de roles.</td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item1" className="mr-2 ml-4 transform scale-150 custom-checkbox" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item2" className="mr-2 ml-4 transform scale-150 custom-checkboxred" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm"><span className="text-red-500 font-bold"></span></td>
+                    
+                    </tr>
+
+                    <tr>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm text-center">1</td>
+                        <td className="px-2 border-2 border-gray-300 text-sm text-center">El software evidencia autenticación y manejo dinámico de roles.</td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item1" className="mr-2 ml-4 transform scale-150 custom-checkbox" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm">
+                        <ul className="list-none">
+                            <li className="flex items-center">
+                            <input type="checkbox" id="item2" className="mr-2 ml-4 transform scale-150 custom-checkboxred" />
+                            </li>
+                        </ul>
+                        </td>
+                        <td className="px-4 py-3 border-2 border-gray-300 text-sm"><span className="text-red-500 font-bold"></span></td>
+                                            
+                    </tr>
+                </tbody>
+                </table>
+            </div>
 
             <div className="flex items-start pt-10 space-x-20">
           <div className="ml-52">
             <span className="text-custom-blue font-semibold text-xl font-inter">Instructor Técnico Anterior</span>
-            <button type="button"onClick={handleUploadPrev}className="flex mt-2 border-2 border-gray-400 bg-zinc-300 shadow-xl rounded-lg text-black text-center font-inter w-52 h-8 cursor-pointer">
+            <button
+              type="button"
+              onClick={handleUploadPrev}
+              className="flex mt-2 border-2 border-gray-400 bg-zinc-300 shadow-xl rounded-lg text-black text-center font-inter w-52 h-8 cursor-pointer"
+            >
               Firma Instructor
             </button>
-            <input type="file"ref={fileInputRefPrev}className="hidden" onChange={(e) => console.log(e.target.files[0])} />
+            <input
+              type="file"
+              ref={fileInputRefPrev}
+              className="hidden"
+              onChange={(e) => console.log(e.target.files[0])} // Aquí puedes manejar el archivo subido
+            />
             <div className="pb-3 border-b-2 border-black w-72 mt-2"></div>
           </div>
 
           <div>
             <span className="text-custom-blue font-semibold text-xl font-inter">Instructor Técnico Nuevo</span>
-            <button type="button" onClick={handleUploadNew}className="flex mt-2 border-2 border-gray-400 bg-zinc-300 shadow-xl rounded-lg text-black text-center font-inter w-52 h-8 cursor-pointer">
+            <button
+              type="button"
+              onClick={handleUploadNew}
+              className="flex mt-2 border-2 border-gray-400 bg-zinc-300 shadow-xl rounded-lg text-black text-center font-inter w-52 h-8 cursor-pointer"
+            >
               Firma Instructor
             </button>
-            <input type="file"ref={fileInputRefNew}className="hidden"onChange={(e) => console.log(e.target.files[0])} />
+            <input
+              type="file"
+              ref={fileInputRefNew}
+              className="hidden"
+              onChange={(e) => console.log(e.target.files[0])} // Aquí puedes manejar el archivo subido
+            />
             <div className="pb-3 border-b-2 border-black w-72 mt-2"></div>
           </div>
 
