3f54ce34bde35a5af66e990989882cddf1f9ea7f
JhorsPineda <jhorspineda30@gmail.com>
2024-09-20 17:45:12 -0500

    actualización de attendanceController

--- aquilesApp/src/main/java/com/senacsf/aquiles/app/controller/AttendancesController.java
+++ aquilesApp/src/main/java/com/senacsf/aquiles/app/controller/AttendancesController.java
@@ -1,21 +1,25 @@
 package com.senacsf.aquiles.app.controller;
 
-import com.google.zxing.WriterException;
-import com.senacsf.aquiles.app.business.AttendancesBusiness;
-import com.senacsf.aquiles.app.dto.AttenancesDto;
-import com.senacsf.aquiles.app.utilities.QrCodeGenerator;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
+import com.google.zxing.WriterException;
+import com.senacsf.aquiles.app.business.AttendancesBusiness;
+import com.senacsf.aquiles.app.dto.AttenancesDto;
+import com.senacsf.aquiles.app.utilities.QrCodeGenerator;
 
 @RestController
 @RequestMapping("/api/attendances")
@@ -50,7 +54,7 @@ public class AttendancesController {
     @GetMapping("/generateQRCode")
     public ResponseEntity<byte[]> generateQRCode() {
         try {
-            String frontendUrl = "  https://260b-152-200-176-22.ngrok-free.app/qrformulariomovil";
+            String frontendUrl = "https://4bc6-152-200-176-22.ngrok-free.app/qrformulariomovil";
             byte[] qrCode = qrCodeGenerator.generateQRCodeImage(frontendUrl);
 
             HttpHeaders headers = new HttpHeaders();
@@ -59,11 +63,5 @@ public class AttendancesController {
         } catch (WriterException | IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-    }
-
-    @GetMapping("/summary/{trainerId}")
-    public ResponseEntity<Map<String, Long>> getAttendanceSummary(@PathVariable Long trainer_id) {
-        Map<String, Long> summary = attendancesBusiness.getAttendanceSummary(trainer_id);
-        return ResponseEntity.ok(summary);
     }
 }
