bf2e2a5d68b222bd3f777856c9bc4199b4f85590
Azkoien07 <caceresgabriel305@gmail.com>
2025-09-04 14:57:26 -0500

    Agregar componente planMejoramientoAprendiz en la ruta de coordinaciones

---
+++ frontend/app/(routes)/dashboard/coordinacionesCoordinador/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import PageTitle from '@components/UI/pageTitle';
+
+const planMejoramientoAprendiz: React.FC = () => {
+    return (
+        <div className="space-y-6">
+            <PageTitle>Mis Coordinaciones</PageTitle>
+        </div>
+    )
+}
+
+export default planMejoramientoAprendiz
