d338cc693d764d484be823771a821abb8a0a7154
JonatanDaza <j0n4t4n.d424@gmail.com>
2025-09-05 15:24:43 -0500

    Creacion de seeders para JustificationStatus, JustificationType y AttendanceState

--- aquilesApi/src/main/java/com/api/aquilesApi/Repository/StateAttendanceRepository.java
+++ aquilesApi/src/main/java/com/api/aquilesApi/Repository/StateAttendanceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StateAttendanceRepository extends JpaRepository<AttendanceState, Long> {
-
+    boolean existsByStatus(String status);
 }
