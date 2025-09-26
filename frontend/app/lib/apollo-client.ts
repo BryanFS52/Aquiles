c3e2b5f6978f05708f3d8644592dc2b32b91964a
Azkoien07 <caceresgabriel305@gmail.com>
2025-09-26 10:55:56 -0500

    Refactor

--- frontend/app/lib/apollo-client.ts
+++ frontend/app/lib/apollo-client.ts
@@ -12,7 +12,7 @@ const client = new ApolloClient({
 // Cliente Lan por medio de la direccion IP
 const clientLAN = new ApolloClient({
     link: new HttpLink({
-        uri: "https://outdoors-assigned-infants-grill.trycloudflare.com/graphql",
+        uri: "http://localhost:4000/graphql",
     }),
     cache: new InMemoryCache({
         addTypename: false,
