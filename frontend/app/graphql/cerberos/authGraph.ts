0097d5a3c55ae3de36afe91186b6269f0a47b0c1
Azkoien07 <caceresgabriel305@gmail.com>
2025-10-27 09:53:36 -0500

    Cambios

---
+++ frontend/app/graphql/cerberos/authGraph.ts
@@ -0,0 +1,92 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Login($input: LoginRequestDTO!) {
+    login(input: $input) {
+      token
+      code
+      message
+      refreshToken
+      user {
+        id
+        roles {
+          name
+        }
+        person {
+          document
+          name
+          photo
+          lastname
+        }
+        processDetails {
+          process {
+            functionName
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const LOGOUT = gql`
+  mutation Logout($idUser: Long, $token: String) {
+    logout(idUser: $idUser, token: $token){
+      code
+      message
+    }
+  }
+`;
+
+export const REFRESH_TOKEN = gql`
+  mutation refreshToken($token: String!) {
+    refreshToken(refreshTokenInput: $token) {
+      refreshToken
+      code
+      message
+    }
+  }
+`;
+
+export const FORGOT_PASSWORD = gql`
+  mutation ForgotPassword($document: Long!) {
+    forgotPassword(document: $document) {
+      code
+      message
+    }
+  }
+`;
+
+export const GET_AUTH_TOKEN = gql`
+  mutation GetAuthToken($token: String!) {
+    getAuthToken(token: $token) {
+      code
+      message
+      token
+      user {
+        id
+        roles {
+          name
+        }
+        person {
+          name
+          photo
+          lastname
+        }
+        processDetails {
+          process {
+            functionName
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const UPDATE_PASSWORD = gql`
+  mutation UpdatePassword($input: UpdatePasswordInputDTO!) {
+    updatePassword(input: $input) {
+      code
+      message
+    }
+  }
+`;
